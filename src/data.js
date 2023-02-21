2a59796925cd97206818ad0d8e158ae97a5b81c1
ashley <alandis01@gmail.com>
2023-02-20 22:14:16 -0500

    pulled in different challenges in data.js along with visuals for each

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "Employee Tracker",
+      subtitle: "Server",
+      description:
+        "Employee Tracker Application",
+      image: "./EmployeeTracker - 1.png",
+      link: "https://github.com/alandis01/EmployeeTracker",
+    },
+    {
+      title: "Team Profile Generator",
+      subtitle: "CLI",
+      description:
+        "An application created that uses the CLI to generate an HTML page containing employee data and their respective data.",
+      image: "./team-profile-generator",
+      link: "https://github.com/alandis01/team-profile-generator",
+    },
+    {
+      title: "Work Day Scheduler",
+      subtitle: "Vanilla JavaScript",
+      description:
+        "An application that allows the user to create and delete notes that will change color based on the current time.",
+      image: "./work-day-scheduler.png",
+      link: "https://alandis01.github.io/work-day-scheduler/",
+    },
+    {
+      title: "Social Network API",
+      subtitle: "",
+      description:
+        "An application that works with Insomnia to show different ways of creating thoughts, users, friends, and reactions as well as updating and deleting.",
+      image: "./18-Social-NetworkAPIScreenshot.png",
+      link: "https://github.com/alandis01/Social-NetworkAPI",
+    },
+  ];
